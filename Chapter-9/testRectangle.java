e6fcbea74ff4c1d5a6456e514df44a712d2a1960
4ethosjustice-arch <4ethosjustice@gmail.com>
2025-09-21 14:50:28 -0600

    Add files via upload

---
+++ Chapter-9/testRectangle.java
@@ -0,0 +1,41 @@
+public class testRectangle {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(4, 40);
+        Rectangle r2 = new Rectangle(3.5, 35.9);
+
+        System.out.println("Rectangle 1:");
+        System.out.println("Width: " + r1.width);
+        System.out.println("Height: " + r1.height);
+        System.out.println("Area: " + r1.getArea());
+        System.out.println("Perimeter: " + r1.getPerimeter());
+
+        System.out.println();
+
+        System.out.println("Rectangle 2:");
+        System.out.println("Width: " + r2.width);
+        System.out.println("Height: " + r2.height);
+        System.out.println("Area: " + r2.getArea());
+        System.out.println("Perimeter: " + r2.getPerimeter());
+    }
+}
+
+class Rectangle {
+    double width = 1;
+    double height = 1;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
