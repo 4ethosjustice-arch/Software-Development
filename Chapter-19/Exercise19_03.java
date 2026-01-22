74a065cc2efe272384e57a86ad8d922e511f2857
4ethosjustice-arch <4ethosjustice@gmail.com>
2026-01-22 05:53:04 -0700

    Add files via upload

---
+++ Chapter-19/Exercise19_03.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Exercise19_03 {
+
+    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+        ArrayList<E> result = new ArrayList<>();
+
+        for (E element : list) {
+            if (!result.contains(element)) {
+                result.add(element);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(14);
+        list.add(24);
+        list.add(14);
+        list.add(42);
+        list.add(25);
+
+        ArrayList<Integer> newList = removeDuplicates(list);
+        System.out.print(newList);
+    }
+}
---
+++ Chapter-19/Exercise19_09.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Exercise19_09 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(14);
+        list.add(24);
+        list.add(4);
+        list.add(42);
+        list.add(5);
+
+        Exercise19_09.<Integer>sort(list);
+
+        System.out.print(list);
+    }
+
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(j).compareTo(list.get(minIndex)) < 0) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                E temp = list.get(i);
+                list.set(i, list.get(minIndex));
+                list.set(minIndex, temp);
+            }
+        }
+    }
+}
